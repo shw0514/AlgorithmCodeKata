9af39b2c4230b902e2c938e92fffffbe6dae98fa
shw0514 <154570818+shw0514@users.noreply.github.com>
2024-01-12 08:54:09 +0900

    [level Lv.0] Title: 나이 출력, Time: 0.14 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120820. 나이 출력/나이 출력.cs
+++ 프로그래머스/Lv.0/120820. 나이 출력/나이 출력.cs
@@ -2,12 +2,9 @@ using System;
 
 public class Solution {
     public int solution(int age) {
-        
-        int koreanAge = age - 1;
-        
         if (age >= 0 && age <= 120)
         {
-            int answer = 2022 - koreanAge;
+            int answer = 2022 - age + 1;
             return answer;
         }
         return -1;
