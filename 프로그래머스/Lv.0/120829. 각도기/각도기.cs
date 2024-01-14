3b993039f65a984d152773252f6bbb3f6d4d7e5c
shw0514 <154570818+shw0514@users.noreply.github.com>
2024-01-15 08:59:33 +0900

    [level Lv.0] Title: 각도기, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120829. 각도기/각도기.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution {
+    public int solution(int angle) {
+        if (angle > 0 && angle <= 180)
+        {
+            
+            if (angle > 0 && angle < 90)
+            {
+                int answer = 1;
+                return answer;
+            }
+            else if (angle == 90)
+            {
+                int answer = 2;
+                return answer;
+            }
+            else if (angle > 90 && angle < 180)
+            {
+                int answer = 3;
+                return answer;
+            }
+            else 
+            {
+                int answer = 4;
+                return answer;
+            }
+            
+        }
+        return -1;
+    }
+}
