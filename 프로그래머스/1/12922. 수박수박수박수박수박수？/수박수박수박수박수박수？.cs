027824dc4d0aee1ad353b6fbc55b250e141ec532
shw0514 <154570818+shw0514@users.noreply.github.com>
2024-01-26 09:49:26 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.20 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        for (int i = 0; i < n; i++)
+        {
+            if(i % 2 != 0)
+            {
+                answer += "박";
+            }
+            else
+            {
+                answer += "수";
+            }
+            
+        }
+        return answer;
+    }
+}
