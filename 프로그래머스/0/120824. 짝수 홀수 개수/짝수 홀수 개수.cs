fcf31f007d3e53def9996f1404981c372a93c0b7
shw0514 <154570818+shw0514@users.noreply.github.com>
2024-07-31 00:32:55 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            if (num_list[i] % 2 == 0) a++;
+            else b++;
+        }
+        int[] answer = {a,b};
+        return answer;
+    }
+}
