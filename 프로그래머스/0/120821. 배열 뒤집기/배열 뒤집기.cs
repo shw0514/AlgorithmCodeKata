651c09f0ec6f6072ca810ec44ccd3c0a17109256
shw0514 <154570818+shw0514@users.noreply.github.com>
2024-07-31 00:22:26 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.39 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        Array.Reverse(num_list);
+        int[] result = num_list;
+        return result;
+    }
+}
